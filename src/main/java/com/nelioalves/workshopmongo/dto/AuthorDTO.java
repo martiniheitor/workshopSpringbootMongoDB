5d0c0a5ddb6ce0af4635e74924f14ab7adaa457c
Heitor Martini <martiniheitor@gmail.com>
2024-11-04 21:42:22 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/nelioalves/workshopmongo/dto/AuthorDTO.java
+++ src/main/java/com/nelioalves/workshopmongo/dto/AuthorDTO.java
@@ -1,25 +1,23 @@
-//Classe para transferência de dados dos usuários, omitindo informações sensíveis ou desnecessárias.
+//Classe para transferência de dados dos autores dos posts, omitindo informações sensíveis ou desnecessárias.
 package com.nelioalves.workshopmongo.dto;
 
 import java.io.Serializable;
 
 import com.nelioalves.workshopmongo.domain.User;
 
-public class UserDTO implements Serializable{
+public class AuthorDTO implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
 	private String id;
 	private String name;
-	private String email;
 	
-	public UserDTO() {
+	public AuthorDTO() {;
 	}
 	
-	public UserDTO(User obj) {
+	public AuthorDTO(User obj) {
 		id = obj.getId();
 		name = obj.getName();
-		email = obj.getEmail();
 	}
 
 	public String getId() {
@@ -36,14 +34,6 @@ public class UserDTO implements Serializable{
 
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
 	}
 
 }
